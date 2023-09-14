7c74d6aa47dc9daf3395970aa0cd2b2135834b57
diegoguillen70 <diegoguillen70@gmail.com>
2023-09-14 23:43:45 +0000

    Models looks like is done [D

--- src/app.py
+++ src/app.py
@@ -9,43 +9,133 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url.replace(
+        "postgres://", "postgresql://"
+    )
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+
 # generate sitemap with all your endpoints
-@app.route('/')
+@app.route("/")
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# GET Methods
+@app.route("/users", methods=["GET"])
+def users_get():
+    response_body = {"msg": "Hello, this is your GET /user response "}
 
     return jsonify(response_body), 200
 
+
+@app.route("/users/favorites", methods=["GET"])
+def favorites_get():
+    response_body = {"msg": "Hello, this is your GET /user/favorites response "}
+
+    return jsonify(response_body), 200
+
+
+@app.route("/people", methods=["GET"])
+def people_get():
+    response_body = {"msg": "Hello, this is your GET /people response "}
+    return jsonify(response_body), 200
+
+
+@app.route("/people/<int:person_id>", methods=["GET"])
+def person_get(person_id):
+    response_body = {"msg": f"Hello, this is your GET /people/{person_id} response "}
+    return jsonify(response_body), 200
+
+
+@app.route("/planets", methods=["GET"])
+def planets_get():
+    response_body = {"msg": "Hello, this is your GET /planets response "}
+    return jsonify(response_body), 200
+
+
+@app.route("/planets/<int:planet_id>", methods=["GET"])
+def planet_get(planet_id):
+    response_body = {"msg": f"Hello, this is your GET /people/{planet_id} response "}
+    return jsonify(response_body), 200
+
+
+@app.route("/starships", methods=["GET"])
+def starships_get():
+    response_body = {"msg": "Hello, this is your GET /planets response "}
+    print("Hello")
+    return jsonify(response_body), 200
+
+
+@app.route("/starships/<int:starship_id>", methods=["GET"])
+def starship_get(starship_id):
+    response_body = {
+        "msg": f"Hello, this is your GET /starships/{starship_id} response "
+    }
+    return jsonify(response_body), 200
+
+
+# POST Methods
+@app.route("/users/favorites/planets/<int:planet_id>", methods=["POST"])
+def add_planets():
+    response_body = request.json
+
+    return jsonify(response_body), 200
+
+
+@app.route("/users/people/<int:people_id>", methods=["POST"])
+def add_people():
+    response_body = request.json
+
+
+@app.route("/users/startships/<int:startship_id>", methods=["POST"])
+def add_startships():
+    response_body = request.json
+
+    return jsonify(response_body), 200
+
+
+# DELETE Methods
+@app.route("/planet/<int:position>", methods=["DELETE"])
+def delete_planet(position):
+    print("delete planets in position " + position)
+    return jsonify(position)
+
+
+@app.route("/people/<int:position>", methods=["DELETE"])
+def delete_person(position):
+    print("delete people in position " + position)
+    return jsonify(position)
+
+
+@app.route("/startships/<int:position>", methods=["DELETE"])
+def delete_startship(position):
+    print("delete starships in position " + position)
+    return jsonify(position)
+
+
 # this only runs if `$ python src/app.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+if __name__ == "__main__":
+    PORT = int(os.environ.get("PORT", 3110))
+    app.run(host="0.0.0.0", port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -2,14 +2,39 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+join_table_people = db.Table(
+    "join_table_people",
+    db.Column("people_id", db.Integer, db.ForeignKey("people.id")),
+    db.Column("favorites_id", db.Integer, db.ForeignKey("favorites.id")),
+)
+
+join_table_planets = db.Table(
+    "join_table_planets",
+    db.Column("planets_id", db.Integer, db.ForeignKey("planets.id")),
+    db.Column("favorites_id", db.Integer, db.ForeignKey("favorites.id")),
+)
+
+join_table_starships = db.Table(
+    "join_table_starships",
+    db.Column("starships_id", db.Integer, db.ForeignKey("starships.id")),
+    db.Column("favorites_id", db.Integer, db.ForeignKey("favorites.id")),
+)
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lasttname = db.Column(db.String(50), nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    favorites = db.relationship("Favorites", backref="user", uselist=False)
+
     def __repr__(self):
-        return '<User %r>' % self.username
+        return "<User %r>" % self.username
 
     def serialize(self):
         return {
@@ -17,3 +42,50 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class People(db.Model):
+    __tablename__ = "people"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    birth_year = db.Column(db.String(15), nullable=False)
+    eye_color = db.Column(db.String(15), nullable=False)
+    gender = db.Column(db.String(25), nullable=False)
+    hair_color = db.Column(db.String(15), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    starships = db.Column(db.String(15), nullable=False)
+    home_world = db.Column(db.String(20), nullable=False)
+
+
+class Favorites(db.Model):
+    __tablename__ = "favorites"
+    id = db.Column(db.Integer, primary_key=True)
+    favorite_characters = db.Column(db.String(200))
+    favorite_planets = db.Column(db.String(200))
+    favorite_starships = db.Column(db.String(200))
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    people = db.relationship("People", secondary=join_table_people)
+    planets = db.relationship("Planets", secondary=join_table_planets)
+    starships = db.relationship("Starships", secondary=join_table_starships)
+
+
+class Planets(db.Model):
+    __tablename__ = "planets"
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    terrain = db.Column(db.String(30), nullable=False)
+    gravity = db.Column(db.String(3), nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    population = db.Column(db.String(250))
+    climate = db.Column(db.String(30))
+
+
+class Starships(db.Model):
+    __tablename__ = "starships"
+    id = db.Column(db.Integer, primary_key=True)
+    population = db.Column(db.String(250))
+    atmosphere = db.Column(db.String(250))
